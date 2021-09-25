74e04746407ec43f0e9bac09ecf56c4507a4b063
BisiDavi <oludavidconnect@gmail.com>
2021-09-25 09:37:42 +0100

    redux files updated

---
+++ store/actions/addProductAction.ts
@@ -0,0 +1,24 @@
+import {productPayloadType, productType} from '@customTypes/addProductTypes';
+import {ADD_PRODUCT_STEP_1} from '@store/constant';
+
+export const AddProductStep1Action =
+  (payload: productPayloadType) =>
+  (
+    dispatch: (arg0: {type: productType; payload: productPayloadType}) => void,
+  ) => {
+    dispatch({
+      type: ADD_PRODUCT_STEP_1,
+      payload,
+    });
+  };
+
+export const AddProductStep2Action =
+  (payload: productPayloadType) =>
+  (
+    dispatch: (arg0: {type: productType; payload: productPayloadType}) => void,
+  ) => {
+    dispatch({
+      type: ADD_PRODUCT_STEP_1,
+      payload,
+    });
+  };
